7a1e612b031522f9debafc7809da70eeeed1bfcf
AdityaTheTechGuy <Adityaguptaofficial1@gmail.com>
2026-06-05 18:54:55 +0530

    Day 5 Completed

--- Week_1/Day_5/Longest_Common_Prefix.cpp
+++ Week_1/Day_5/Longest_Common_Prefix.cpp
@@ -6,13 +6,11 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         if (strs.empty()) return "";
         
-        // Loop through characters of the first string
         for (int i = 0; i < strs[0].length(); i++) {
             char currChar = strs[0][i];
             
-            // Compare this character with the same position in all other strings
             for (int j = 1; j < strs.size(); j++) {
-                // Return if string is shorter or character mismatches
+                
                 if (i == strs[j].length() || strs[j][i] != currChar) {
                     return strs[0].substr(0, i);
                 }
